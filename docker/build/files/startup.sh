4a904600521fff8ba8fc5b5a18d4b380bf28162b
Lothar "Husky110" Hoffmann <l.hoffmann@cherrymint.de>
2023-11-26 22:31:00 +0100

    Clarification

--- docker/build/files/startup.sh
+++ docker/build/files/startup.sh
@@ -4,6 +4,10 @@ if [ ! -f "/var/www/database/database.sqlite" ]; then
 fi
 cd /var/www
 composer update
+if [ ! -f "/var/www/.env" ]; then
+  cp .env.example .env
+  php artisan key:generate
+fi
 php artisan migrate
 php artisan stablecompanion:clear-civitaicache
 php artisan stablecompanion:scan-for-files
