3acc7228462881c264ff2096d1029e8a7f2cc8f4
Lothar "Husky110" Hoffmann <l.hoffmann@cherrymint.de>
2023-12-06 22:49:24 +0100

    Download-Links

--- src/app/Filament/Resources/DownloadResource.php
+++ src/app/Filament/Resources/DownloadResource.php
@@ -62,6 +62,9 @@ class DownloadResource extends Resource
                 Tables\Columns\TextColumn::make('name')
                     ->label('Name')
                     ->getStateUsing(fn($record) => CivitAIConnector::getModelMetaByID($record->civit_id)['name'].' - '.CivitAIConnector::getSpecificModelVersionByModelIDAndVersionID($record->civit_id, $record->version)['name']),
+                Tables\Columns\TextColumn::make('link')
+                    ->label('CivitAI-URL')
+                    ->getStateUsing(fn($record) => new HtmlString('<a href="civitai.com/models/'.$record->civit_id.'?modelVersionId='.$record->version.'" target="_blank">Link</a>')),
                 Tables\Columns\TextColumn::make('type')
                     ->label('Type')
                     ->getStateUsing(fn($record) => strtoupper($record->type)),
@@ -112,7 +115,9 @@ class DownloadResource extends Resource
                 Tables\Actions\DeleteAction::make('delete')
                     ->button()
                     ->action(function ($record){
-                        Aria2Connector::abortDownloadInAria2($record);
+                        if($record->status != 'error'){
+                            Aria2Connector::abortDownloadInAria2($record);
+                        }
                         if($record->existingModel != null){
                             if($record->existingModel->files->count() == 0 && $record->existingModel->activedownloads->count() == 0){
                                 $record->existingModel->deleteModel();
